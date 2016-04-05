3a08ec3478f49ba09f1523310350da189374adbe
DericM <dmccadden@my.bcit.ca>
2016-04-05 16:10:12 -0700

    c

--- src/Q4.java
+++ src/Q4.java
@@ -1,17 +1,15 @@
-
 public class Q4 {
 
-    
-    
     public static void main(String args[]){
-        int n = 12;
+        int n = 20;
         
         int i = 1;
         int j = 3;
         while (i != n){
-            j = 2 * j + i - 1;
+            System.out.print(j +",");
+            j = 2 * j - i + 1;
             i = i + 1;
-            System.out.print("" + j +",");
+            
         }
         
     }
